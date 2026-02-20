13e17e96b5950bf8537599bf0153ac4f8375567f
Dheeraj2521 <polemdheeraj20@gmail.com>
2026-02-20 10:11:29 +0530

    [Huner] Added UC1 WelcomePage

---
+++ PalindromeCheckerAppp/src/App.java
@@ -0,0 +1,17 @@
+/**
+ * @Huner
+ * @version 1.0
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println("   Welcome to Palindrome Checker App   ");
+        System.out.println("========================================");
+        System.out.println("Version: 1.0");
+        System.out.println();
+        System.out.println("Application started successfully.");
+        System.out.println("Palindrome logic will be implemented in the next stage.");
+    }
+}
